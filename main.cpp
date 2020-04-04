2d480082a436fd99b80bd9f1b41bda0ecf873b98
Szymon Chłopek <59609487+IFrost400@users.noreply.github.com>
2020-04-04 18:11:57 +0200

    V2
    Przebudowa programu, dodanie klasy Neuron i Network, dodanie funkcji Forward dla Network

--- main.cpp
+++ main.cpp
@@ -17,18 +17,40 @@ double sigmoid(double x)
 }
 double derivsigmoid(double x)
 {
-    return  sigmoid(x)*(1-sigmoid(x));
+    this->w0=(rand()*1.0)/RAND_MAX;
+    this->w1=(rand()*1.0)/RAND_MAX;
+    this->b=(rand()*1.0)/RAND_MAX;
 }
-double mse_loss(int y[],int ypred[])
-{
-    int avg;
-    for(int i=0;i<(sizeof(y)/sizeof(*y));i++)
+};
+class Network{
+public:
+    Neuron H0,H1,O0;
+    double sigmoid(double x)
     {
-        avg+=(y[i]-ypred[i])^2;
+        return 1/(1+exp(x));
     }
-    return avg/(sizeof(y)/sizeof(*y));
+    double derivsigmoid(double x)
+    {
+        return  sigmoid(x)*(1-sigmoid(x));
+    }
+    double mse_loss(int y[],int ypred[],int size)
+    {
+        int avg;
+        for(int i=0;i<size;i++)
+        {
+            avg+=(y[i]-ypred[i])^2;
+        }
+        return avg/size;
+    }
+double Forward(Neuron N, int X0,int X1)
+{
+    double VH0,VH1,VO0;
+    VH0 = sigmoid(H0.w0*X0+H0.w1*X1);
+    VH1 = sigmoid(H1.w0*X0+H1.w1*X1);
+    VO0 = sigmoid(O0.w0*VH0+O0.w0*VH1);
+    return VO0;
 }
-
+};
 int main()
 {
 
